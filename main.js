a33b6c5ddcc286e6e770f66332b564906678f028
1vlada1 <1vladastojanovic1@gmail.com>
2019-10-03 21:33:19 +0200

    Displayed book on list

--- main.js
+++ main.js
@@ -49,6 +49,21 @@ class UI {
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
-// Event: Add Book
 
+// Event: Add Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+    console.log(book);
+
+    // Add book to list
+    UI.addBookToList(book);
+})
 // Event: Remove a Book
